4529dd795fb4c14eeb44d75eaed3c61f8b013ea7
Lidiya Shulepova <shulepovalidiya@gmail.com>
2023-09-26 23:43:37 +0300

    Updates

--- src/components/input-mask.js
+++
@@ -1,9 +0,0 @@
-import IMask from "imask";
-
-const inputElement = document.querySelector('input[name="telephone"]');
-
-const maskOptions = {
-  mask: '+{7}(000)000-00-00'
-};
-
-IMask(inputElement, maskOptions);
--- src/pages/index.js
+++
@@ -1,6 +0,0 @@
-import './index.sass'
-import '../components/input-mask'
-import '../components/swiper'
-import '../components/accordion'
-import '../components/popup'
-
