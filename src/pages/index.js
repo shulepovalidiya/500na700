8a93f88759493795609608a33d8864225cbab3c0
Lidiya Shulepova <shulepovalidiya@gmail.com>
2023-09-27 14:54:10 +0300

    Fixed menu width

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,3 +3,10 @@ import "../components/input-mask";
 import "../components/swiper";
 import "../components/accordion";
 import "../components/popup";
+
+function _calculateScrollbarWidth() {
+  document.documentElement.style.setProperty('--scrollbar-width', (window.innerWidth - document.documentElement.clientWidth) + "px");
+}
+window.addEventListener('resize', _calculateScrollbarWidth, false);
+document.addEventListener('DOMContentLoaded', _calculateScrollbarWidth, false);
+window.addEventListener('load', _calculateScrollbarWidth);
